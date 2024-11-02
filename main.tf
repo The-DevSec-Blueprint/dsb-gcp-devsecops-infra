10a2cba0eee1561928e827420eef537892026598
Damien J. Burks <damienjburks@Damiens-MacBook-Pro.local>
2024-11-02 15:25:24 -0500

    adding kube configuration

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "google_project" "default" {}
 resource "google_storage_bucket" "default" {
-  name          = "dsb-devsecops-lab"
+  name          = "dsb-devsecops-lab-bucket"
   location      = var.region
   force_destroy = true
 
@@ -9,4 +9,33 @@ resource "google_storage_bucket" "default" {
   }
 
   uniform_bucket_level_access = true
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "dsb-devsecops-cluster"
+  location = var.region
+
+  # Only one node pool with one node
+  initial_node_count = 1
+
+  # Enable GKE features
+  remove_default_node_pool = true
+  deletion_protection = false
+
+  # Specify network and subnetwork
+  network    = "default"
+  subnetwork = "default"
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 1  # Single node in the pool - super cheap :)
+
+  node_config {
+    machine_type = "e2-standard-8"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
 }
